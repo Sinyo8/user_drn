3bf432ed7ce6000af51b75439724fb3dcabf6ff0
dicky rizky <113374439+Sinyo8@users.noreply.github.com>
2023-01-20 16:02:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const article = document.querySelector(".text");
+const server = `      <article class="text">
+<h1>Lately Im Love Study Javascritp</h1>
+<h1>And I love Study All About New Jobdesk</h1>
+<h1>My Hobbies Is Eating</h1>
+</article>`;
